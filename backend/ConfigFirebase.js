73d9b2c533bad729a61ad698085ea5a6349aed74
jorgegoncalves <jorgegoncalves@dcc.ufmg.br>
2019-08-02 15:55:06 -0300

    inicial

---
+++ backend/ConfigFirebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "api-key",
+    authDomain: "project-id.firebaseapp.com",
+    databaseURL: "https://project-id.firebaseio.com",
+    projectId: "project-id",
+    storageBucket: "project-id.appspot.com",
+    messagingSenderId: "sender-id",
+};
